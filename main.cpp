36ef32b97c728e8bbd75ec704461789117c57517
EricMontez <eric.alexander.montez@gmail.com>
2026-03-16 16:58:31 -0700

    A45 Done

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ int main()
     cin >> num1 >> num2 >> num3;
 
     // TODO: Find min, max, and med among num1, num2, num3
+    min = (num1 < num2) ? num1 : num2;
+    min = (min < num3) ? min : num3;
+    max = (num1 > num2) ? num1 : num2;
+    max = (max > num3) ? max : num3;
+    med = num1 + num2 + num3 - min - max;
 
     cout << min << " " << med << " " << max << endl;
 }
